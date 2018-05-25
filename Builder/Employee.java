9efdb66a5d1d3a3ab65ce6e9a120ae339c0bf0df
frandell <bigfrancis10@gmail.com>
2018-05-24 20:51:17 -0400

    Builder

---
+++ Builder/Employee.java
@@ -0,0 +1,50 @@
+public class Employee
+{
+	private String firstName;
+	private String lastName;
+	private int SSN;
+	private int age;
+	private Address address;
+
+	public String getFirstName()
+	{
+		return firstName;
+	}
+	public void setFirstName(String firstName)
+	{
+		this.firstName = firstName;
+	}
+	public String getlastName()
+	{
+		return lastName;
+	}
+	public void setLastName(String lastName)
+	{
+		this.lastName = lastName;
+	}
+	public int getSSN()
+	{
+		return SSN;
+	}
+	public void setSSN(int SSN)
+	{
+		this.SSN = SSN;
+	}
+	public Address getAddress()
+	{
+		return address;
+	}
+	public void setAddress(Address address)
+	{
+		this.address = address;
+	}
+	public int getAge()
+	{
+		return age;
+	}
+	public void setAge(int age)
+	{
+		this.age = age;
+	}
+
+}
